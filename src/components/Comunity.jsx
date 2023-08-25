8a417e3c11018e347e109d7e2344d3667a38faaf
AlanSunny750 <alansunny7509@gmail.com>
2023-08-25 02:56:34 -0700

    final update

--- src/components/Comunity.jsx
+++ src/components/Comunity.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import Navbar from "./Navbar";
 
 function Comunity() {
+    let url = 'http://localhost:3000'
+
     const [user] = useState({
         name: 'User',
         lastName: 'Name',
@@ -19,27 +21,53 @@ function Comunity() {
 
     const [isClicked, setIsClicked] = useState(false);
 
-    const sow = () => {
+    const plant = () => {
         setIsClicked(true);
     }
 
-    const plant = () => {
-        if (newPost.title && newPost.description) {
-            const newPostWithImage = {
-                ...newPost,
-                image: image ? URL.createObjectURL(image) : null
-            };
-            setPosts(prevPosts => [...prevPosts, newPostWithImage]);
-            setIsClicked(false);
-            setNewPost({
-                title: '',
-                description: ''
-            });
-            setImage(null);
-        } else {
-            alert("Please fill in both title and description.");
+        const Submit = async () => {
+            if (newPost.title && newPost.description) {
+                const newPostWithImage = {
+                    ...newPost,
+                    image: image ? URL.createObjectURL(image) : null
+                };
+                setPosts(prevPosts => [...prevPosts, newPostWithImage]);
+                setIsClicked(false);
+                setNewPost({
+                    title: '',
+                    description: ''
+                });
+                setImage(null);
+    
+                // Create a FormData object to send the image
+                const formData = new FormData();
+                formData.append('title', newPost.title);
+                formData.append('description', newPost.description);
+                if (image) {
+                    formData.append('image', image);
+                }
+                console.log(JSON.stringify(formData))
+                // Send a POST request
+                try {
+                    const response = await fetch(url + '/comunity', {
+                        method: 'POST',
+                        body: JSON.stringify(formData)
+                    });
+    
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+    
+                    const responseData = await response.json();
+                    console.log(responseData);
+                } catch (error) {
+                    console.error('There was a problem with the fetch operation:', error.message);
+                }
+    
+            } else {
+                alert("Please fill in both title and description.");
+            }
         }
-    }
 
     return (
         <>
@@ -53,13 +81,6 @@ function Comunity() {
                 <div className="mb-8">
                     <div className="flex justify-between items-center mb-4">
                         <div className="flex gap-4">
-                            <button 
-                                className="bg-amber-700 text-white rounded-md px-4 py-2 hover:bg-amber-800 transition duration-150"
-                                onClick={sow}
-                            > 
-                                Sow
-                            </button>
-
                             <button 
                                 className="bg-green-500 text-white rounded-md px-4 py-2 hover:bg-green-600 transition duration-150"
                                 onClick={plant}
@@ -96,7 +117,7 @@ function Comunity() {
                                 onChange={e => setImage(e.target.files[0])}
                                 className="mb-2"
                             />
-                            <button onClick={plant} className="bg-blue-500 text-white rounded-md px-4 py-2 hover:bg-blue-600 transition duration-150">Submit</button>
+                            <button onClick={Submit} className="bg-blue-500 text-white rounded-md px-4 py-2 hover:bg-blue-600 transition duration-150">Submit</button>
                         </div>
                     )}
 
